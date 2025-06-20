0baba3b237fb3ea95ad6fa5d93e17d25b8f13cbc
Jorge Melegati <jorge@jmelegati.com>
2025-06-20 16:55:38 +0200

    Fixing one project and adding more.

--- projects/Compromise.py
+++ projects/Compromise.py
@@ -2,19 +2,18 @@ from interfaces.ProjectInterface import ProjectInterface
 from interfaces.TestError import TestError
 from interfaces.LintError import LintError
 from helpers.ProjectHelper import (
-    install_npm_packages, fix_eslint_issues,
-    get_eslint_errors, get_mocha_errors,
+    get_tap_errors, install_npm_packages, 
+    fix_eslint_issues, get_eslint_errors
 )
 
-
-class Supertest(ProjectInterface):
+class Compromise(ProjectInterface):
     @property
     def path(self):
-        return 'repos/supertest'
+        return 'repos/compromise'
 
     @property
     def code_dir(self):
-        return '/'
+        return '/src'
 
     def after_copy_hook(self, path_suffix) -> None:
         project_copy_path = self.path + path_suffix
@@ -26,14 +25,16 @@ class Supertest(ProjectInterface):
         return fixed_code
 
     def get_lint_errors(self):
-        lint_command = 'npx eslint lib/**/*.js test/**/*.js index.js'
+        lint_command = 'npx eslint src'
         errors = get_eslint_errors(self.dirty_path, lint_command)
 
         return errors
 
     def get_test_errors(self):
-        test_command = 'npx run mocha'
-        line_pattern = r' *at Object.<anonymous> \(\S+dayjs\D+:(\d+):\d+\)'
+        test_command = 'npx tape "./tests/**/*.test.js"'
+        line_pattern = r'TestContext.<anonymous> \((\S+underscore\D+):(\d+):\d+\)'
+        errors = get_tap_errors(self.dirty_path, test_command, line_pattern)
 
-        errors = get_mocha_errors(self.dirty_path, test_command, line_pattern)
         return errors
+        
+        
--- projects/Yargs.py
+++ projects/Yargs.py
@@ -3,18 +3,18 @@ from interfaces.TestError import TestError
 from interfaces.LintError import LintError
 from helpers.ProjectHelper import (
     install_npm_packages, fix_eslint_issues,
-    get_eslint_errors, get_mocha_errors,
+    get_eslint_errors, get_mocha_errors, 
 )
 
 
-class Supertest(ProjectInterface):
+class Yargs(ProjectInterface):
     @property
     def path(self):
-        return 'repos/supertest'
+        return 'repos/yargs'
 
     @property
     def code_dir(self):
-        return '/'
+        return '/lib'
 
     def after_copy_hook(self, path_suffix) -> None:
         project_copy_path = self.path + path_suffix
@@ -26,14 +26,14 @@ class Supertest(ProjectInterface):
         return fixed_code
 
     def get_lint_errors(self):
-        lint_command = 'npx eslint lib/**/*.js test/**/*.js index.js'
+        lint_command = 'npx eslint .'
         errors = get_eslint_errors(self.dirty_path, lint_command)
 
         return errors
 
     def get_test_errors(self):
-        test_command = 'npx run mocha'
-        line_pattern = r' *at Object.<anonymous> \(\S+dayjs\D+:(\d+):\d+\)'
+        test_command = 'npx mocha --enable-source-maps ./test/*.mjs --require ./test/before.mjs' 
+        line_pattern = r' *at Context.<anonymous> \(\D+:(\d+):\d+\)'
 
         errors = get_mocha_errors(self.dirty_path, test_command, line_pattern)
         return errors
